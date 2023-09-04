b59459ebb06489dc7eda71e62da729a845fee040
grimpatron <Hellhammer220bpm@gmail.com>
2023-09-04 11:46:18 +0300

    make a task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,23 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.minValue = min;
+        this.maxValue = max;
     }
 
     guess() {
-
+        this.currentValue = this.maxValue - this.minValue;
+        this.currentValue = Math.floor(this.currentValue / 2);
+        this.currentValue = this.maxValue - this.currentValue;
+        return this.currentValue;
     }
 
     lower() {
-
+        this.maxValue = this.currentValue;
     }
 
     greater() {
-
+        this.minValue = this.currentValue;
     }
 }
 

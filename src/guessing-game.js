86f68c5a0347959157a3d703e6e7735ae306952a
grimpatron <hellhammer220bpm@gmail.com>
2024-09-02 14:32:55 +0300

    feat: finish task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,25 +1,48 @@
 class GuessingGame {
-    constructor() {}
+  constructor() {}
 
-    setRange(min, max) {
-        this.minValue = min;
-        this.maxValue = max;
-    }
+  setRange(min, max) {
+    (this.minValue = min), (this.maxValue = max);
+  }
 
-    guess() {
-        this.currentValue = this.maxValue - this.minValue;
-        this.currentValue = Math.floor(this.currentValue / 2);
-        this.currentValue = this.maxValue - this.currentValue;
-        return this.currentValue;
-    }
+  guess() {
+    this.currentValue = Math.floor((this.maxValue - this.minValue) / 2);
+    this.currentValue = this.maxValue - this.currentValue;
+    return this.currentValue;
+  }
 
-    lower() {
-        this.maxValue = this.currentValue;
-    }
+  lower() {
+    this.maxValue = this.currentValue;
+  }
 
-    greater() {
-        this.minValue = this.currentValue;
-    }
+  greater() {
+    this.minValue = this.currentValue;
+  }
 }
-
 module.exports = GuessingGame;
+
+// class GuessingGame {
+//     constructor() {}
+
+//     setRange(min, max) {
+//         this.minValue = min;
+//         this.maxValue = max;
+//     }
+
+//     guess() {
+//         this.currentValue = this.maxValue - this.minValue;
+//         this.currentValue = Math.floor(this.currentValue / 2);
+//         this.currentValue = this.maxValue - this.currentValue;
+//         return this.currentValue;
+//     }
+
+//     lower() {
+//         this.maxValue = this.currentValue;
+//     }
+
+//     greater() {
+//         this.minValue = this.currentValue;
+//     }
+// }
+
+// module.exports = GuessingGame;
